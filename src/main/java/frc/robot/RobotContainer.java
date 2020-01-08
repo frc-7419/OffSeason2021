bddbf79268578cf164b869dcdfbf0d2635e1e7e4
Neha Pant <npant03@gmail.com>
2020-01-07 18:16:40 -0800

    add limelight subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,12 +12,14 @@ import com.team7419.PaddedXbox;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.dashboard.Dashboard;
 import frc.robot.subsystems.drive.*;
+import frc.robot.subsystems.vision.LimelightSub;
 
 public class RobotContainer {
 
   private final DriveBaseSub driveBase = new DriveBaseSub();
   private final Dashboard dashboard = new Dashboard();
   private final PaddedXbox joystick = new PaddedXbox();
+  private final LimelightSub limelight = new LimelightSub();
 
   private final ArcadeDrive arcade = new ArcadeDrive(joystick, driveBase, .4, .4);
 
---
+++ src/main/java/frc/robot/subsystems/vision/LimelightSub.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSub extends SubsystemBase{
+
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+
+    public LimelightSub(){}
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("tx", tx.getDouble(0));
+        SmartDashboard.putNumber("ty", ty.getDouble(0));
+    }
+}
