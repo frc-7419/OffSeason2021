1df78801af056f3ea66b101daf0f290b15b3108a
Neha Pant <npant03@gmail.com>
2020-01-22 17:32:59 -0800

    add commands to run intake

---
+++ src/main/java/frc/robot/subsystems/intake/RunLoader.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RunLoader extends CommandBase{
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private LoaderSub loader;
+  private double power;
+
+
+  public RunLoader(LoaderSub loader, double power) {
+    this.loader = loader;
+    this.power = power;
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+    loader.setPower(power);
+}
+
+  @Override
+  public void end(boolean interrupted) {
+      loader.setPower(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+
+}
