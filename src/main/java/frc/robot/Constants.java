5aa06985f29b58b5501c37ca2f744ed5f867401b
Neha Pant <npant03@gmail.com>
2020-02-03 11:03:36 -0800

    add 254 esque map

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import java.util.ArrayList;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -39,4 +41,29 @@ public final class Constants {
     public static class RobotConstants{
         public static double kCameraHeight = 10;
     }
+
+    public static double[][] kSpeedToFf = {
+        {1000, .09},
+        {2000, .065},
+        {3000, .0575},
+        {4000, .0545},
+        {5000, .0525},
+        {6000, .051},
+        {7000, .0505},
+        {8000, .0495},
+        {9000, .049},
+        {10000, .0485},
+        {11000, .0482},
+        {12000, .0478},
+        {13000, .0475},
+        {14000, .0474},
+        {15000, .0474},
+        {16000, .0474},
+        {17000, .0472},
+        {18000, .047},
+        {19000, .047},
+        {20000, .047},
+        {21000, .0472},
+        {22000, .0472},
+    };
 }
