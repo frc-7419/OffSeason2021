77e3e4d37d8cb6660831c435b05bbf36338aa443
Neha Pant <npant03@gmail.com>
2020-02-28 13:31:09 -0800

    clean up, more conducive to pr

--- src/main/java/frc/robot/subsystems/autos/FaceplantThenShoot.java
+++ src/main/java/frc/robot/subsystems/autos/FaceplantThenShoot.java
@@ -3,21 +3,16 @@ package frc.robot.subsystems.autos;
 import com.team7419.Sleep;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.buttons.ReadyToShoot;
-import frc.robot.subsystems.buttons.RunShooter;
-import frc.robot.subsystems.dashboard.Dashboard;
+import frc.robot.subsystems.buttons.*;
 import frc.robot.subsystems.drive.*;
 import frc.robot.subsystems.intake.LoaderSub;
 import frc.robot.subsystems.intake.RevolverSub;
-import frc.robot.subsystems.sensors.*;
 import frc.robot.subsystems.shooter.*;
 
 public class FaceplantThenShoot extends SequentialCommandGroup {
   
   private DriveBaseSub driveBase;
   private ShooterSub shooter;
-  private Dashboard dashboard;
-  private RevColorDistanceSub colorSensor;
   private RevolverSub revolver;
   private LoaderSub loader;
 
@@ -25,10 +20,8 @@ public class FaceplantThenShoot extends SequentialCommandGroup {
     
     driveBase = robot.getDriveBase();
     shooter = robot.getShooter();
-    dashboard = robot.getDashboard();
     revolver = robot.getRevolver();
     loader = robot.getLoader();
-    colorSensor = robot.getColorSensor();
 
     addCommands(new StraightWithMotionMagic(driveBase, 24));
     addCommands(new StraightWithMotionMagic(driveBase, -9));
