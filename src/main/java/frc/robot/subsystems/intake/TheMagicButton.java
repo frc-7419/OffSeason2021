48295e96b06210f6f234035e0a53851bc6d05c80
Neha Pant <npant03@gmail.com>
2020-03-02 09:36:58 -0800

    remove comments

--- src/main/java/frc/robot/subsystems/intake/TheMagicButton.java
+++ src/main/java/frc/robot/subsystems/intake/TheMagicButton.java
@@ -12,9 +12,7 @@ import com.team7419.PaddedXbox;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.intake.IntakeSub;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+
 public class TheMagicButton extends SequentialCommandGroup {
   private final PaddedXbox joystick = new PaddedXbox();
   private final RevolverSub revolver = new RevolverSub();
