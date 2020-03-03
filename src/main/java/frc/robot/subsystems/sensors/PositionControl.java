13d2c97bb49f994f29912d79b0f7ef0376a96308
Ayush Bajaj <ayush.b@icloud.com>
2020-03-02 17:06:27 -0800

    Added Driverstation/FMS Communication

--- src/main/java/frc/robot/subsystems/sensors/PositionControl.java
+++ src/main/java/frc/robot/subsystems/sensors/PositionControl.java
@@ -16,12 +16,12 @@ public class PositionControl extends CommandBase {
   private final ColorSensor colorSensor;
   public String FMSColor;
   public boolean colorFound;
+  public String processedColor;
   /**
    * Creates a new RotationControl.
    */
-  public PositionControl(ColorSensor colorSensor, String color) {
+  public PositionControl(ColorSensor colorSensor) {
     this.colorSensor = colorSensor;
-    FMSColor = color;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(colorSensor);
   }
@@ -30,6 +30,31 @@ public class PositionControl extends CommandBase {
   @Override
   public void initialize() {
     colorFound = false;
+    FMSColor = DriverStation.getInstance().getGameSpecificMessage();
+
+    if(FMSColor.length() > 0) {
+      switch (FMSColor.charAt(0)) {
+        case 'B' :
+          processedColor = "Blue";
+          break;
+        case 'G' :
+          processedColor = "Green";
+          break;
+        case 'R' :
+          processedColor = "Red";
+          break;
+        case 'Y' :
+          processedColor = "Yellow";
+          break;
+        default :
+        SmartDashboard.putString("Position Control Color", "Data Not Recognized");
+          break;
+      }
+    } else {
+      SmartDashboard.putString("Position Control Color", "Data Not Found");
+    }
+
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -46,7 +71,7 @@ public class PositionControl extends CommandBase {
     String[] colorArray = {"Red", "Yellow", "Blue", "Green", "Red", "Yellow", "Blue", "Green"}; 
 
     for (int i = 4; i < colorArray.length; i++) {
-      if (colorArray[i].equalsIgnoreCase(FMSColor)) {
+      if (colorArray[i].equalsIgnoreCase(processedColor)) {
         sensorColor = colorArray[i-2];
         SmartDashboard.putString("SENSOR COLOR", sensorColor);
         break; 
