40f289679b48212d575f368d138d101e0b84c10c
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2021-10-26 20:56:52 -0700

    Committing changes to my Auton

--- src/main/java/frc/robot/subsystems/autos/sudyAuton.java
+++ src/main/java/frc/robot/subsystems/autos/sudyAuton.java
@@ -5,7 +5,9 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.PowerConstants;
 import frc.robot.subsystems.buttons.*;
 import frc.robot.subsystems.drive.DriveBaseSub;
+import frc.robot.subsystems.drive.StraightPowerTime;
 import frc.robot.subsystems.drive.StraightWithMotionMagic;
+import frc.robot.subsystems.drive.TurnPowerTime;
 import frc.robot.subsystems.intake.LoaderSub;
 import frc.robot.subsystems.intake.RevolveWithIntake;
 import frc.robot.subsystems.intake.RevolverSub;
@@ -16,9 +18,7 @@ import frc.robot.subsystems.shooter.ShooterSub;
 
 public class sudyAuton extends SequentialCommandGroup {
     public sudyAuton(DriveBaseSub driveBaseSub, ShooterSub shooterSub, RevolverSub revolver, RevColorDistanceSub colorSensor, LoaderSub loader) {
-        addCommands(new StraightWithMotionMagic(driveBaseSub, 120));
-        addCommands(new WaitCommand(2));
-        addCommands(new RunLoader(loader, 100, false));
-        addCommands(new RunRevolver(revolver, 100, false));
+        addCommands(new StraightPowerTime(driveBaseSub, 0.5, 1000));
+        addCommands(new StraightPowerTime(driveBaseSub, -0.5, 1000));
     }
 }
