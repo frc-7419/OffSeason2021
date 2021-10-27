ad0a8f7e7d460d904e6689a7035bb8889c91739e
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2021-10-27 00:35:01 -0700

    Auton Code complete

--- src/main/java/frc/robot/subsystems/autos/sudyAuton.java
+++ src/main/java/frc/robot/subsystems/autos/sudyAuton.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.drive.DriveBaseSub;
 import frc.robot.subsystems.drive.StraightPowerTime;
 import frc.robot.subsystems.drive.StraightWithMotionMagic;
 import frc.robot.subsystems.drive.TurnPowerTime;
+import frc.robot.subsystems.drive.DriveBaseSub.TurnDirection;
 import frc.robot.subsystems.intake.LoaderSub;
 import frc.robot.subsystems.intake.RevolveWithIntake;
 import frc.robot.subsystems.intake.RevolverSub;
@@ -17,9 +18,11 @@ import frc.robot.subsystems.sensors.RevColorDistanceSub;
 import frc.robot.subsystems.shooter.ShooterSub;
 
 public class sudyAuton extends SequentialCommandGroup {
-    public sudyAuton(DriveBaseSub driveBaseSub, ShooterSub shooterSub, RevolverSub revolver, RevColorDistanceSub colorSensor, LoaderSub loader) {
+    public sudyAuton(DriveBaseSub driveBaseSub) {
         addCommands(new StraightPowerTime(driveBaseSub, 0.5, 1000));
+        addCommands(new TurnPowerTime(driveBaseSub, 0.5, TurnDirection.RIGHT, 1000));
         addCommands(new StraightPowerTime(driveBaseSub, -0.5, 1000));
+        addCommands(new TurnPowerTime(driveBaseSub, 0.5, TurnDirection.LEFT, 1000));
         //Testing this
     }
 }
