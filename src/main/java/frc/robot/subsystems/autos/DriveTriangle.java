76cda512d55811b3f02ead931ff7ca881b3f2b32
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-10-29 12:22:28 -0700

    Automous Test

---
+++ src/main/java/frc/robot/subsystems/autos/DriveTriangle.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drive.DriveBaseSub;
+import frc.robot.subsystems.drive.GyroSub;
+import frc.robot.subsystems.drive.StraightPowerTime;
+import frc.robot.subsystems.drive.TurnWithGyro;
+
+public class StephenAutonTest extends SequentialCommandGroup {
+
+  public DriveTriangle(DriveBaseSub driveBase) {
+    addCommands(new StraightPowerTime(driveBase, 0.5, 1000));
+    addCommands(new TurnWithGyro(driveBase, ahrs, 120));
+    addCommands(new StraightPowerTime(driveBase, 0.5, 1000));
+    addCommands(new TurnWithGyro(driveBase, ahrs, 120));
+    addCommands(new StraightPowerTime(driveBase, 0.5, 1000));
+  }
+}
